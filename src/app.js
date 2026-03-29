b298643f02c3f876da9c28570ffef701bdb51211
PauDev1 <pa.o.liz666@gmail.com>
2026-03-29 19:29:26 -0300

    chore: add morgan logger

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import productRoutes from './routes/productRoutes.js';
 
 const app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/products', productRoutes);
 
