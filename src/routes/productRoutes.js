b7cd7c645a67c647d18fc845aabbe50b9c05eb15
PauDev1 <pa.o.liz666@gmail.com>
2026-04-01 22:10:19 -0300

    feat: implement auth middleware and protect product routes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { protect } from '../middleware/authMiddleware.js';
 
 import {
     getProducts,
@@ -12,8 +13,8 @@ const router = express.Router();
 
 router.get('/', getProducts);
 router.get('/:id', getProductById);
-router.post('/', createProduct);    
-router.put('/:id', updateProduct);  
-router.delete('/:id', deleteProduct); 
+router.post('/', protect, createProduct);    
+router.put('/:id', protect, updateProduct);  
+router.delete('/:id', protect, deleteProduct); 
 
 export default router;
